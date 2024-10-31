3fa7ad5795c3bc401cb4bbebf5d5ce1881cab7aa
Maffe <matiaspeltomaayo@gmail.com>
2024-10-31 17:22:09 +0200

    user story 3 complete

--- piglatin.py
+++ piglatin.py
@@ -9,3 +9,20 @@ class PigLatin:
     def translate(self) -> str:
         if not self.phrase:
             return "nil"
+
+    def translate_word(self, word: str) -> str:
+        vowels = "aeiouAEIOU"
+
+        if word[0] in vowels:
+            if word[-1] == "y":
+                return word + "nay"
+            elif word[-1] in vowels:
+                return word + "yay"
+            else:
+                return word + "ay"
+
+        else:
+            for i, letter in enumerate(word):
+                if letter in vowels:
+                    return word[i:] + word [:i] + "ay"
+            return word + "ay"
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -13,3 +13,15 @@ class TestPigLatin(unittest.TestCase):
     def test_empty_phrase(self):
         plt = PigLatin("")
         self.assertEqual(plt.translate(), "nil")
+
+    def test_translate_word_start_vowel_end_y(self):
+        plt = PigLatin("any")
+        self.assertEqual(plt.translate_word("any"), "anynay")
+
+    def test_translate_word_start_vowel_end_vowel(self):
+        plt = PigLatin("apple")
+        self.assertEqual(plt.translate_word("apple"), "appleyay")
+
+    def test_translate_word_start_vowel_end_consonant(self):
+        plt = PigLatin("ask")
+        self.assertEqual(plt.translate_word("ask"), "askay")
