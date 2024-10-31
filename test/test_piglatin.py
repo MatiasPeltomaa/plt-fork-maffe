fabf9963e634992082d04be95816ecfb03249ffa
Maffe <matiaspeltomaayo@gmail.com>
2024-10-31 16:59:30 +0200

    user story 1 complete

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -5,5 +5,7 @@ from error import PigLatinError
 
 class TestPigLatin(unittest.TestCase):
 
-    def test_something(self):
-        pass
+    def test_init_and_get_phrase(self):
+        phrase = "hello world"
+        plt = PigLatin(phrase)
+        self.assertEqual(plt.get_phrase(), phrase)
